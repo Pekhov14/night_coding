21c5a1203265a420c2d1cd48655eaf9d3635d7ca
Pekhov <pehovanton21@gmail.com>
2019-08-09 03:44:19 +0300

    Функция sum в функциональном стиле

--- fun_sum.js
+++ fun_sum.js
@@ -1,12 +1,8 @@
 function sum() {
-    let result = 0;
-    let elements = Array.from(arguments); // в elements > массив из арбументов
-    for(let i = elements.length - 1; i >= 0; i--) {
-        result += elements[i];
-    }
-    return result;
+    return Array.from(arguments).reduce((a, b) => a + b, 0);
 }
 
-
+// Spec
+console.log(sum());
+console.log(sum(1));
 console.log(sum(1, 2));
-console.log(sum(1, 2, 3));
