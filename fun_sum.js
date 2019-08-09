f01117704913c2ec25fe72a45d37e2f28d57a15c
Pekhov <pehovanton21@gmail.com>
2019-08-09 04:25:05 +0300

    Добавление функций очистки и верефикации

--- fun_sum.js
+++ fun_sum.js
@@ -1,8 +1,23 @@
+function clean(elements) {
+    return elements.filter((el) => !isNaN(Number(el)));
+}
+
+function verify(elements) {
+    elements.forEach((el) => {if (isNaN(Number(el))) throw new Error("Not a number")})
+    return elements;
+}
+
 function sum() {
-    return Array.from(arguments).reduce((a, b) => a + b, 0);
+    return clean(Array.from(arguments)).reduce((a, b) => Number(a) + Number(b), 0);
 }
 
 // Spec
 console.log(sum());
 console.log(sum(1));
 console.log(sum(1, 2));
+
+// Gigo
+console.log(sum('1', '1'));
+console.log(sum('as', '1')); // clean, 1
+console.log(sum([], {}, '1', 1)); // 2
+console.log(sum([1], {}, '1', 1)); // 3
