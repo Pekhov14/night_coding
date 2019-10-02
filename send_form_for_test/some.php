30c94c092438c666115f54650f0d6d34d569d3c0
Pekhov Anton <pehovanton21@gmail.com>
2019-10-02 14:58:50 +0300

    Update some.php

--- send_form_for_test/some.php
+++ send_form_for_test/some.php
@@ -5,14 +5,9 @@ $data = [
     "userphone" => $_POST['userphone']
 ];
 
-$connection = new PDO('mysql:host=127.0.0.1:3306;dbname=pekhov', 'pekhov', 'pekhovanton');
+$connection = new PDO('mysql:host=127.0.0.1;dbname=pekhov', '*', '*');
 
 // INSERT
 $sql = 'INSERT INTO test (name, phone) VALUES (:username, :userphone)';
 $statement = $connection->prepare($sql);
 $statement->execute($data);
-
-
-
-
-
