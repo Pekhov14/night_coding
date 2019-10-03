555b707550e929a1be11c01913d9008895b1c284
Pekhov <pehovanton21@gmail.com>
2019-10-03 10:21:32 +0300

    add index

--- DI/index.php
+++ DI/index.php
@@ -1,1 +1,12 @@
 <?php
+
+require_once 'classes/Connection.php';
+require_once 'classes/QueryBuilder.php';
+
+
+$db = new QueryBuilder(Connection::make());
+
+$users = $db->select('users');
+$posts = $db->select('posts');
+
+var_dump($users, $posts);
