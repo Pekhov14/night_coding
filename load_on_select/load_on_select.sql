d8b3560e197aa2dcb17f692eb8b2e121cb9d485b
Pekhov <pehovanton21@gmail.com>
2020-01-26 17:52:50 +0300

    add sql damp

---
+++ load_on_select/load_on_select.sql
@@ -0,0 +1,109 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Янв 26 2020 г., 17:50
+-- Версия сервера: 10.3.13-MariaDB-log
+-- Версия PHP: 7.3.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `load_on_select`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `brand`
+--
+
+CREATE TABLE `brand` (
+  `brand_id` int(11) NOT NULL,
+  `brand_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `brand`
+--
+
+INSERT INTO `brand` (`brand_id`, `brand_name`) VALUES
+(1, 'Samsung'),
+(2, 'Sony'),
+(3, 'Motorola'),
+(4, 'Xiaomi');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `product`
+--
+
+CREATE TABLE `product` (
+  `product_id` int(11) NOT NULL,
+  `product_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `brand_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `product`
+--
+
+INSERT INTO `product` (`product_id`, `product_name`, `brand_id`) VALUES
+(1, 'Samsung Galaxy A9', 1),
+(2, 'Samsung Galaxy S7', 1),
+(3, 'Samsung Galaxy S6 edge', 1),
+(4, 'Xperia Z5 Premium', 2),
+(5, 'Xperia M5 Dual', 2),
+(6, 'Xperia C5 uplt', 2),
+(7, 'Moto G Turbo', 3),
+(8, 'Moto X Force', 3),
+(9, 'Redmi 3 Pro', 4),
+(10, 'Mi 5', 4);
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `brand`
+--
+ALTER TABLE `brand`
+  ADD PRIMARY KEY (`brand_id`);
+
+--
+-- Индексы таблицы `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`product_id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `brand`
+--
+ALTER TABLE `brand`
+  MODIFY `brand_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT для таблицы `product`
+--
+ALTER TABLE `product`
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
